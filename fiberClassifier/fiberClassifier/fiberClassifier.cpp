e52f60c5a0a778d8425a0c2cd0ae554a372167af
Rubén Domínguez Falcón <nabariho@gmail.com>
2014-03-24 16:33:51 +0000

    ..

--- fiberClassifier/fiberClassifier/fiberClassifier.cpp
+++ fiberClassifier/fiberClassifier/fiberClassifier.cpp
@@ -24,16 +24,18 @@ int main( int argc, char** argv )
   String file1 = "C:/Users/nabar/Documents/GitHub/Fiber-Classifier/fiberClassifier/images/4_3_1.tif";
   String file2 = "C:/Users/nabar/Documents/GitHub/Fiber-Classifier/fiberClassifier/images/4_50_1.tif";
   String file3 = "C:/Users/nabar/Documents/GitHub/Fiber-Classifier/fiberClassifier/images/10_3_1.tif";
-  src = imread(file2);
+  src = imread(file1);
 
   /// Convert image to gray and blur it
   cvtColor( src, src_gray, CV_BGR2GRAY );
+  blur( src_gray, src_gray, Size(3,3) );
+  equalizeHist(src_gray,src_gray);
   blur( src_gray, src_gray, Size(3,3) );
 
   /// Create Window
   char* source_window = "Source";
   namedWindow( source_window, CV_WINDOW_AUTOSIZE );
-  imshow( source_window, src );
+  imshow( source_window, src_gray );
 
   createTrackbar( " Canny thresh:", "Source", &thresh, max_thresh, thresh_callback );
   thresh_callback( 0, 0 );
@@ -50,7 +52,7 @@ void thresh_callback(int, void* )
   vector<Vec4i> hierarchy;
 
   /// Detect edges using canny
-  Canny( src_gray, canny_output, thresh, thresh*2, 3 );
+  Canny( src_gray, canny_output, thresh, thresh*2, 5 );
   /// Find contours
   findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
 
