3af94060aba07f12363c8471b193798f54eaca1a
Rubén Domínguez Falcón <nabariho@gmail.com>
2014-04-07 19:31:48 +0100

    init

--- fiberClassifier/fiberClassifier/fiberClassifier.cpp
+++ fiberClassifier/fiberClassifier/fiberClassifier.cpp
@@ -14,47 +14,58 @@
 using namespace cv;
 using namespace std;
 
-Mat getContours(string File1, int cannyThreshold){
-	Mat src,blurMat,canny_output;
-	src = imread( File1);
+#include "stdafx.h"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-	/// Convert image to gray and blur it
-	cvtColor( src, blurMat, CV_BGR2GRAY );
-	blur( blurMat, blurMat, Size(3,3) );
-	equalizeHist(blurMat,blurMat);
 
-	vector<vector<Point> > contours;
-	vector<Vec4i> hierarchy;
 
-	Canny( blurMat, canny_output, cannyThreshold, cannyThreshold*3, 3 );
-	//bitwise_not(canny_output,canny_output);
-	//findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-	//Mat drawing = Mat::zeros(blurMat.size(), CV_8UC3 );
+using namespace cv;
+using namespace std;
 
-	//for( int i = 0; i< contours.size(); i++ )
- //    {
-	//	drawContours( drawing, contours, i, 255, CV_FILLED, 8, hierarchy, 0, Point() );
- //    }
-	//return drawing;
-	bitwise_not(canny_output, canny_output);
-	return canny_output;
-}
 
-Mat getContourFromBinary(Mat canny_output)
+
+int edgeThresh = 1;
+int lowThreshold = 0;
+int const max_lowThreshold = 500;
+int ratio = 3;
+int kernel_size = 3;
+char* window_name = "Edge Map";
+
+
+/**
+ * @function CannyThreshold
+ * @brief Trackbar callback - Canny thresholds input with a ratio 1:3
+ */
+Mat CannyThreshold(Mat src, int, void*)
 {
+ 
+ Mat src_gray;
+ Mat dst, detected_edges, inv;
+ dst.create( src.size(), src.type() );
+ inv.create( src.size(), src.type() );
 
-	vector<vector<Point> > contours;
-	vector<Vec4i> hierarchy;
+	
+ /// Reduce noise with a kernel 3x3
+  blur( src_gray, detected_edges, Size(3,3) );
+  equalizeHist(detected_edges,detected_edges);
 
-	findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-	Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3 );
+  /// Canny detector
+ Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
 
-	for( int i = 0; i< contours.size(); i++ )
-     {
-		drawContours( drawing, contours, i, 255, CV_FILLED, 8, hierarchy, 0, Point() );
-     }
-	return drawing;
-}
+  /// Using Canny's output as a mask, we display our result
+  dst = Scalar::all(0);
+  src.copyTo( dst, detected_edges);
+  src.copyTo(inv, detected_edges);
+  blur(inv,inv, Size(3,3));
+  bitwise_not(inv,inv);
+
+  return inv;
+ }
+
 
 
 int _tmain(int argc, _TCHAR* argv[])
@@ -64,85 +75,67 @@ int _tmain(int argc, _TCHAR* argv[])
 	String file2 = "C:/Users/nabar/Documents/GitHub/Fiber-Classifier/fiberClassifier/images/4_50_1.tif";
 	String file3 = "C:/Users/nabar/Documents/GitHub/Fiber-Classifier/fiberClassifier/images/10_3_1.tif";
 
+	  /// Create a matrix of the same type and size as src (for dst)
+
+
+
 
 	Sample my_sample(file1,file2,file3,BLACK_THRESHOLD_IMAGE1,BLACK_THRESHOLD_IMAGE2,BLACK_THRESHOLD_IMAGE3);
 	my_sample.align(); //Queda pendiente tratar el alineamiento
 	Mat channels[3],channel;
 	
-	Image blackImage1, blackImage2,blackImage3;
+	Mat blackImage1, blackImage2,blackImage3;
 
-	blackImage1 = my_sample.images[0].get_black();
-	blackImage2 = my_sample.images[1].get_black();
-	blackImage3 = my_sample.images[2].get_black();
+	blackImage1 = CannyThreshold(my_sample.images[0].image_mat(), 0, 0);
+	blackImage2 = CannyThreshold(my_sample.images[1].image_mat(), 0, 0);
+	blackImage3 = CannyThreshold(my_sample.images[2].image_mat(), 0, 0);
+
 
 
 	Mat invBlackImage1, invBlackImage2, invBlackImage3;
 	Mat type1, type2, type3;
 
-	bitwise_not(blackImage1.image_mat(),invBlackImage1);
-	bitwise_not(blackImage2.image_mat(),invBlackImage2);
-	bitwise_not(blackImage3.image_mat(),invBlackImage3);
+	bitwise_not(blackImage1,invBlackImage1);
+	bitwise_not(blackImage2,invBlackImage2);
+	bitwise_not(blackImage3,invBlackImage3);
+
+	namedWindow( "Negras1",CV_WINDOW_NORMAL);// Create a window for display.
+	imshow( "Negras1", blackImage1);	
+	namedWindow( "Negras2",CV_WINDOW_NORMAL);// Create a window for display.
+	imshow( "Negras2", blackImage2);	
+	namedWindow( "Negras3",CV_WINDOW_NORMAL);// Create a window for display.
+	imshow( "Negras3", blackImage3);	
 
 
 
-	//Fibras tipo 1 (1, 1, 0)
-	bitwise_and(invBlackImage1,invBlackImage2,type1);
-	bitwise_and(blackImage3.image_mat(),type1,type1);
+	////Fibras tipo 1 (1, 1, 0)
+	//bitwise_and(invBlackImage1,invBlackImage2,type1);
+	//bitwise_and(blackImage3,type1,type1);
 
-	namedWindow( "Tipo1",CV_WINDOW_NORMAL);// Create a window for display.
-	imshow( "Tipo1", type1);
+	//namedWindow( "Tipo1",CV_WINDOW_NORMAL);// Create a window for display.
+	//imshow( "Tipo1", type1);
 
-	//Fibras tipo 2 (1, 0, 1)
-	bitwise_and(invBlackImage1, blackImage2.image_mat(), type2);
-	bitwise_and(type2, invBlackImage3, type2);
+	////Fibras tipo 2 (1, 0, 1)
+	//bitwise_and(invBlackImage1, blackImage2, type2);
+	//bitwise_and(type2, invBlackImage3, type2);
 
-	namedWindow( "Tipo2",CV_WINDOW_NORMAL);// Create a window for display.
-	imshow( "Tipo2", type2);
+	//namedWindow( "Tipo2",CV_WINDOW_NORMAL);// Create a window for display.
+	//imshow( "Tipo2", type2);
 
-	//Fibras tipo 3 (0, 1, 1)
-	bitwise_and(blackImage1.image_mat(), invBlackImage2, type3);
-	bitwise_and(type3,invBlackImage3,type3);
+	////Fibras tipo 3 (0, 1, 1)
+	//bitwise_and(blackImage1, invBlackImage2, type3);
+	//bitwise_and(type3,invBlackImage3,type3);
 
-	namedWindow( "Tipo3",CV_WINDOW_NORMAL);// Create a window for display.
-	imshow( "Tipo3", type3);
-
-
-	channels[0] = type1;
-	channels[1] = type2;
-	channels[2] = type3;
-	merge(channels, 3, channel);
-	namedWindow( "combined",CV_WINDOW_NORMAL);// Create a window for display.
-	imshow( "combined", channel);
-
-  /// Show in a window
-	Mat contourOriginal1 = getContours(file1, CANNY_THRESHOLD_IMAGE1);
-	Mat contourOriginal2 = getContours(file2, CANNY_THRESHOLD_IMAGE2);
-	Mat contourOriginal3 = getContours(file3, CANNY_THRESHOLD_IMAGE3);
-
-	Mat contourType1, contourType2, contourType3;
-
-	namedWindow( "Contornos1",CV_WINDOW_NORMAL);// Create a window for display.
-	imshow( "Contornos1", contourOriginal1);
-	namedWindow( "Contornos2",CV_WINDOW_NORMAL);// Create a window for display.
-	imshow( "Contornos2", contourOriginal2);
-	namedWindow( "Contornos3",CV_WINDOW_NORMAL);// Create a window for display.
-	imshow( "Contornos3", contourOriginal3);
-
-	contourType1 = getContourFromBinary(type1);
-	namedWindow( "Contornos Tipo1",CV_WINDOW_NORMAL);// Create a window for display.
-	imshow( "Contornos Tipo1", contourType1);
-
-	contourType2 = getContourFromBinary(type2);
-	namedWindow( "Contornos Tipo2",CV_WINDOW_NORMAL);// Create a window for display.
-	imshow( "Contornos Tipo2", contourType2);
-
-	contourType3 = getContourFromBinary(type3);
-	namedWindow( "Contornos Tipo3",CV_WINDOW_NORMAL);// Create a window for display.
-	imshow( "Contornos Tipo3", contourType3);
+	//namedWindow( "Tipo3",CV_WINDOW_NORMAL);// Create a window for display.
+	//imshow( "Tipo3", type3);
 
 
-
-
+	//channels[0] = type1;
+	//channels[1] = type2;
+	//channels[2] = type3;
+	//merge(channels, 3, channel);
+	//namedWindow( "combined",CV_WINDOW_NORMAL);// Create a window for display.
+	//imshow( "combined", channel);
 
     waitKey();
     return 0;
